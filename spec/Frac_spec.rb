9bf9da514e03c8fb0a0634d1d2d7cefcf7122d84
Nati <alu0100695595@ull.edu.es>
2013-10-24 15:45:31 +0100

    Solucionadas expectativas de los métodos num, denom y forma reducida, y añadidas expectativas de mostrado en fraccion y en flotante

--- spec/Frac_spec.rb
+++ spec/Frac_spec.rb
@@ -8,18 +8,18 @@ describe Fraccion do
 
 describe "Racional" do
     it "Existe un numerador" do
-      @f1.n.should_not be_nil
-      @f2.n.should_not be_nil
+      @f1.num().should_not be_nil
+      @f2.num().should_not be_nil
     end
     it "Existe un denominador" do
-      @f1.d.should_not be_nil
-      @f2.d.should_not be_nil
+      @f1.denom().should_not be_nil
+      @f2.denom().should_not be_nil
     end
     it "Debe de estar en su forma reducida" do
-      @f1.n.should eq(3)
-      @f1.d.should eq(8)
-      @f2.n.should eq(1)
-      @f2.d.should eq(8)
+      @f1.num().should eq(3)
+      @f1.denom().should eq(8)
+      @f2.num().should eq(1)
+      @f2.denom().should eq(8)
       
     end
     it "Se debe invocar al metodo num() para obtener el numerador" do
@@ -29,6 +29,14 @@ describe "Racional" do
     it "Se debe invocar al metodo denom() para obtener el denominador" do
       @f1.denom().should eq(@f1.d)
       @f2.denom().should eq(@f2.d)
+    end
+    it "Se debe mostrar por la consola la fraccion de la forma: a/b, donde a es el numerador y b el denominador" do
+      @f1.to_s().should eq("3/8")
+      @f2.to_s().should eq("1/8") 
+    end
+    it "Se debe mostrar por la consola la fraccion en formato flotante" do
+      @f1.float().should eq(0.375)
+      @f2.float().should eq(0.125)
     end
   end
 end
